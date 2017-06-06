f9698466ef1860d30b1093a1839fa689f7ea613d
Manu shrivastava <shrivastava.manu87@gmail.com>
2017-06-06 16:45:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/manushrivastava/fileupload/MainActivity.java
@@ -0,0 +1,183 @@
+package com.example.manushrivastava.fileupload;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PICKFILE_RESULT_CODE = 1;
+    static String FilePath="";
+    UploadFileAsync u;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("*/*");
+        startActivityForResult(intent,PICKFILE_RESULT_CODE);
+    }
+    public void upload(View view)
+    {
+        TextView t=(TextView)findViewById(R.id.manu);
+        t.setText(FilePath);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch(requestCode){
+            case PICKFILE_RESULT_CODE:
+                if(resultCode==RESULT_OK){
+
+                    FilePath = data.getData().getPath();
+                    FilePath=FilePath.split(":")[1];
+                     u=new UploadFileAsync();
+                     u.execute("");
+
+                }
+                break;
+
+        }
+    }
+}
+class UploadFileAsync extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        try {
+            String sourceFileUri ="/sdcard/"+MainActivity.FilePath;
+
+            HttpURLConnection conn = null;
+            DataOutputStream dos = null;
+            String lineEnd = "\r\n";
+            String twoHyphens = "--";
+            String boundary = "*****";
+            int bytesRead, bytesAvailable, bufferSize;
+            byte[] buffer;
+            int maxBufferSize = 1 * 1024 * 1024;
+            File sourceFile = new File(sourceFileUri);
+
+            if (sourceFile.isFile()) {
+
+                try {
+                    String upLoadServerUri = "http://192.168.43.8/FileUpload.php";
+
+                    // open a URL connection to the Servlet
+                    FileInputStream fileInputStream = new FileInputStream(
+                            sourceFile);
+                    URL url = new URL(upLoadServerUri);
+
+                    // Open a HTTP connection to the URL
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoInput(true); // Allow Inputs
+                    conn.setDoOutput(true); // Allow Outputs
+                    conn.setUseCaches(false); // Don't use a Cached Copy
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Connection", "Keep-Alive");
+
+                    conn.setRequestProperty("ENCTYPE",
+                            "multipart/form-data");
+                    conn.setRequestProperty("Content-Type",
+                            "multipart/form-data;boundary=" + boundary);
+                    conn.setRequestProperty("bill", sourceFileUri);
+
+                    dos = new DataOutputStream(conn.getOutputStream());
+
+                    dos.writeBytes(twoHyphens + boundary + lineEnd);
+                    dos.writeBytes("Content-Disposition: form-data; name=\"bill\";filename=\""
+                            + sourceFileUri + "\"" + lineEnd);
+
+                    dos.writeBytes(lineEnd);
+
+                    // create a buffer of maximum size
+                    bytesAvailable = fileInputStream.available();
+
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    buffer = new byte[bufferSize];
+
+                    // read file and write it into form...
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                    while (bytesRead > 0) {
+
+                        dos.write(buffer, 0, bufferSize);
+                        bytesAvailable = fileInputStream.available();
+                        bufferSize = Math
+                                .min(bytesAvailable, maxBufferSize);
+                        bytesRead = fileInputStream.read(buffer, 0,
+                                bufferSize);
+
+                    }
+
+                    // send multipart form data necesssary after file
+                    // data...
+                    dos.writeBytes(lineEnd);
+                    dos.writeBytes(twoHyphens + boundary + twoHyphens
+                            + lineEnd);
+
+                    // Responses from the server (code and message)
+
+                    int serverResponseCode = conn.getResponseCode();
+                    String serverResponseMessage = conn
+                            .getResponseMessage();
+
+                    if (serverResponseCode == 200) {
+
+                        // messageText.setText(msg);
+                      //  Toast.makeText(, "File Upload Complete.",Toast.LENGTH_SHORT).show();
+
+                        // recursiveDelete(mDirectory1);
+
+                    }
+
+                    // close the streams //
+                    fileInputStream.close();
+                    dos.flush();
+                    dos.close();
+
+                } catch (Exception e) {
+
+
+                    e.printStackTrace();
+
+                }
+                // dialog.dismiss();
+
+            } // End else block
+
+
+        } catch (Exception ex) {
+            // dialog.dismiss();
+
+            ex.printStackTrace();
+        }
+        return "Executed";
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+    }
+}
