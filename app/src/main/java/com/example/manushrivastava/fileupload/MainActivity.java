41b4d4b203b21dc7a5d1096b2cae29f7fc956afb
Manu shrivastava <shrivastava.manu87@gmail.com>
2017-06-07 10:07:32 +0530

    final

--- app/src/main/java/com/example/manushrivastava/fileupload/MainActivity.java
+++ app/src/main/java/com/example/manushrivastava/fileupload/MainActivity.java
@@ -75,72 +75,41 @@ class UploadFileAsync extends AsyncTask<String, Void, String> {
 
                 try {
                     String upLoadServerUri = "http://192.168.43.8/FileUpload.php";
-
-                    // open a URL connection to the Servlet
-                    FileInputStream fileInputStream = new FileInputStream(
-                            sourceFile);
+                    FileInputStream fileInputStream = new FileInputStream(sourceFile);
                     URL url = new URL(upLoadServerUri);
-
-                    // Open a HTTP connection to the URL
                     conn = (HttpURLConnection) url.openConnection();
-                    conn.setDoInput(true); // Allow Inputs
-                    conn.setDoOutput(true); // Allow Outputs
-                    conn.setUseCaches(false); // Don't use a Cached Copy
                     conn.setRequestMethod("POST");
                     conn.setRequestProperty("Connection", "Keep-Alive");
-
-                    conn.setRequestProperty("ENCTYPE",
-                            "multipart/form-data");
-                    conn.setRequestProperty("Content-Type",
-                            "multipart/form-data;boundary=" + boundary);
-                    conn.setRequestProperty("bill", sourceFileUri);
+                    conn.setRequestProperty("Content-Type","multipart/form-data;boundary=" + boundary);
 
                     dos = new DataOutputStream(conn.getOutputStream());
-
                     dos.writeBytes(twoHyphens + boundary + lineEnd);
-                    dos.writeBytes("Content-Disposition: form-data; name=\"bill\";filename=\""
-                            + sourceFileUri + "\"" + lineEnd);
-
+                    dos.writeBytes("Content-Disposition: form-data; name=\"bill\";filename=\""+ sourceFileUri + "\"" + lineEnd);
                     dos.writeBytes(lineEnd);
 
                     // create a buffer of maximum size
                     bytesAvailable = fileInputStream.available();
-
                     bufferSize = Math.min(bytesAvailable, maxBufferSize);
                     buffer = new byte[bufferSize];
 
                     // read file and write it into form...
                     bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
                     while (bytesRead > 0) {
-
                         dos.write(buffer, 0, bufferSize);
                         bytesAvailable = fileInputStream.available();
-                        bufferSize = Math
-                                .min(bytesAvailable, maxBufferSize);
-                        bytesRead = fileInputStream.read(buffer, 0,
-                                bufferSize);
-
+                        bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                        bytesRead = fileInputStream.read(buffer, 0,bufferSize);
                     }
 
-                    // send multipart form data necesssary after file
-                    // data...
                     dos.writeBytes(lineEnd);
-                    dos.writeBytes(twoHyphens + boundary + twoHyphens
-                            + lineEnd);
+                    dos.writeBytes(twoHyphens + boundary + twoHyphens+ lineEnd);
 
-                    // Responses from the server (code and message)
 
                     int serverResponseCode = conn.getResponseCode();
-                    String serverResponseMessage = conn
-                            .getResponseMessage();
+                    String serverResponseMessage = conn.getResponseMessage();
 
                     if (serverResponseCode == 200) {
 
-                        // messageText.setText(msg);
-                      //  Toast.makeText(, "File Upload Complete.",Toast.LENGTH_SHORT).show();
-
-                        // recursiveDelete(mDirectory1);
 
                     }
 
